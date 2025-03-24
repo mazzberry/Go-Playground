e8e96166c854d05d2e814981b204e1c06a960dda
Mazzberry <h.mohammadreza736@gmail.com>
2025-03-24 22:55:54 +0330

    add basic data types examples for int, float, rune, string, and type alias in Go

--- 02.Values/main.go
+++ 02.Values/main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
+	"unsafe"
 )
 
 func main() {
 	var r rune = 'k'
 
 	fmt.Printf("%c %T %U", r, r, r)
+	var a = bits.UintSize
+	fmt.Println(a)
+
+	var fnum int
+	var fnum8 int8
+	var fnum16 int16
+	var fnum32 int32
+	var fnum64 int64
+
+	
+
+	fmt.Printf("num %d bytes\n", unsafe.Sizeof(fnum)) 
+	fmt.Printf("num8 %d bytes\n", unsafe.Sizeof(fnum8))
+	fmt.Printf("num16 %d bytes\n", unsafe.Sizeof(fnum16))
+	fmt.Printf("num32 %d bytes\n", unsafe.Sizeof(fnum32)) 
+	fmt.Printf("num64 %d bytes\n", unsafe.Sizeof(fnum64))
+
+
+	var floatnum32 float32 
+	var floatnum64 float64 
+
+	fmt.Printf("floatnum32 %d bytes\n", unsafe.Sizeof(floatnum32))
+	fmt.Printf("floatnum64 %d bytes\n", unsafe.Sizeof(floatnum64))
+	
+
+	
 
 }
