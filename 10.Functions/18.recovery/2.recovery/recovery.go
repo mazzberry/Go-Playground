72b2ee7a195b936809b600bb55467ea43ac89d30
Mazzberry <h.mohammadreza736@gmail.com>
2025-03-24 05:15:04 +0330

    add recovery mechanism and Persian README for stack trace details

--- 10.Functions/18.recovery/2.recovery/recovery.go
+++ 10.Functions/18.recovery/2.recovery/recovery.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func main() {
+
+	a := []string{"a", "b"}
+	checkAndPrint(a, 2)
+
+}
+
+func checkAndPrint(s []string, index int) {
+	defer handelOutBounds()
+
+	if index > (len(s) - 1) {
+		panic("out of bound access for slice")
+	}
+	fmt.Println(s[index])
+
+}
+
+func handelOutBounds() {
+
+	if r := recover(); r != nil {
+		fmt.Println("Recovering form panic :", r)
+		fmt.Println("stack trace :")
+		debug.PrintStack()
+	}
+
+}
