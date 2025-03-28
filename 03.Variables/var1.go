7e48ed8e2139cfa8d2d198388b645042e86f612a
Mazzberry <h.mohammadreza736@gmail.com>
2025-03-28 05:13:46 +0330

    refactor print statements for consistency in var1.go

--- 03.Variables/var1.go
+++ 03.Variables/var1.go
@@ -9,7 +9,7 @@ func main() {
 	// 2
 	var a string
 	a = "Hello world_2"
-	fmt.Println(a, "\n")
+	fmt.Println(a)
 	// 3
 	var i3 = 3
 	var f1 = 3.44
@@ -19,19 +19,19 @@ func main() {
 	fmt.Printf("%v, %T\n", s1, s1)
 	// 4
 	b := "hello word 3 in short way" // this is only works in function (its not public variable)
-	fmt.Println(b, "\n")
-	// 5 
+	fmt.Println(b)
+	// 5
 	n, k, l := "hello", 1, 1.5 // define a few variables in one line
 	var p, o, i = "world", 13, 24
-	fmt.Println(n, k, l, p, o, i, "\n")
-	// 6 
+	fmt.Println(n, k, l, p, o, i)
+	// 6
 
- var (
-	 i7 = 12 
-	 f5 = 3.78 
-	 s2 = "hello world"  // for public variables we use this
- )
-    fmt.Printf("%v, %T\n", i7, i7)
+	var (
+		i7 = 12
+		f5 = 3.78
+		s2 = "hello world" // for public variables we use this
+	)
+	fmt.Printf("%v, %T\n", i7, i7)
 	fmt.Printf("%v, %T\n", f5, f5)
 	fmt.Printf("%v, %T\n", s2, s2)
 
