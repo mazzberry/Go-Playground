61ab34ae77a412d96c131228fd448a3625bcb375
Mazzberry <h.mohammadreza736@gmail.com>
2025-03-28 05:12:59 +0330

    refactor variable declarations and print statements for clarity in var1.go

--- 03.Variables/var1.go
+++ 03.Variables/var1.go
@@ -3,19 +3,37 @@ package main
 import "fmt"
 
 func main() {
+	// 1
 	var s string = "Hello world" // for public variables we use this
 	fmt.Printf(s, "\n")
-
+	// 2
 	var a string
 	a = "Hello world_2"
-	fmt.Printf(a, "\n")
-
+	fmt.Println(a, "\n")
+	// 3
+	var i3 = 3
+	var f1 = 3.44
+	var s1 = "Hello world" // for public
+	fmt.Printf("%v, %T\n", i3, i3)
+	fmt.Printf("%v, %T\n", f1, f1)
+	fmt.Printf("%v, %T\n", s1, s1)
+	// 4
 	b := "hello word 3 in short way" // this is only works in function (its not public variable)
-	fmt.Printf(b, "\n")
-
+	fmt.Println(b, "\n")
+	// 5 
 	n, k, l := "hello", 1, 1.5 // define a few variables in one line
 	var p, o, i = "world", 13, 24
-	fmt.Printf(n, k, l, p, o, i, "\n")
+	fmt.Println(n, k, l, p, o, i, "\n")
+	// 6 
+
+ var (
+	 i7 = 12 
+	 f5 = 3.78 
+	 s2 = "hello world"  // for public variables we use this
+ )
+    fmt.Printf("%v, %T\n", i7, i7)
+	fmt.Printf("%v, %T\n", f5, f5)
+	fmt.Printf("%v, %T\n", s2, s2)
 
 	var y int
 	var r float32
