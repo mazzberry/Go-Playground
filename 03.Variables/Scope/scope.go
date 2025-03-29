019a4c58db658cd4d91410e7e5387514862f35d7
Mazzberry <h.mohammadreza736@gmail.com>
2025-03-29 07:56:23 +0330

    add: introduce a second global variable and demonstrate variable shadowing in scope.go

--- 03.Variables/Scope/scope.go
+++ 03.Variables/Scope/scope.go
@@ -4,13 +4,17 @@ import "fmt"
 
 
 var glob = 125 // glob is global to this package
+var glob2 = 250 // glob2 is global to this package
 
 func main() {
+	fmt.Println(glob) // 125
+	var glob2 = 300 // glob2 is local to this main function and shadows glob2
+	fmt.Println(glob2)
 
 	{
 		//var x = 14 // x is local to this block
 	}
-	fmt.Println(glob) // 125
+	
 	//fmt.Println(x) // 14 // Error: x is not defined in this scope
 
 }
