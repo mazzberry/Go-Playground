be519ada356b38d551d1f14d5d668cfe15aa0f74
Mazzberry <h.mohammadreza736@gmail.com>
2025-03-30 09:54:43 +0330

    refactor: rename global variables for clarity and demonstrate variable shadowing in scope.go

--- 03.Variables/Scope/scope.go
+++ 03.Variables/Scope/scope.go
@@ -2,19 +2,18 @@ package main
 
 import "fmt"
 
-
-var glob = 125 // glob is global to this package
-var glob2 = 250 // glob2 is global to this package
+var globalBlock = 125 // globalBlock is a global variable to this package
 
 func main() {
-	fmt.Println(glob) // 125
-	var glob2 = 300 // glob2 is local to this main function and shadows glob2
-	fmt.Println(glob2)
+	fmt.Println(globalBlock) // 125
+
+	{
+		var globalBlock = 200    // we overshadow the globalBlock with a local one in this block
+		fmt.Println(globalBlock) // 200
+	}
 
 	{
 		//var x = 14 // x is local to this block
 	}
-	
-	//fmt.Println(x) // 14 // Error: x is not defined in this scope
 
 }
