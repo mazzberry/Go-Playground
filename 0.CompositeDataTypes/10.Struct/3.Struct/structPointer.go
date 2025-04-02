080a1de7b5fe36ad887723c2e125ede153f42ba5
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-02 08:11:23 +0330

    add: implement employee struct with pointer demonstration in structPointer.go

---
+++ 0.CompositeDataTypes/10.Struct/3.Struct/structPointer.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type employee struct {
+	name   string
+	age    int
+	salary float32
+}
+
+func main() {
+	
+	emp := employee{name: "Mohammad", age: 21, salary: 15000.0}
+	empP := &emp // pointer to the struct
+	fmt.Printf("Emp: %+v\n", empP)
+	empP = &employee{name: "John Doe", age: 30, salary: 1000.0} // change the value of the struct through the pointer
+	fmt.Printf("Emp: %+v\n", empP)
+
+
+}
