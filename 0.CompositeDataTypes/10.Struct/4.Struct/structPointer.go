02a8a8c58f7f033c1d9641b2c293e2f5c214d073
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-03 18:42:36 +0330

    add: modify employee struct values through pointer in structPointer.go

--- 0.CompositeDataTypes/10.Struct/4.Struct/structPointer.go
+++ 0.CompositeDataTypes/10.Struct/4.Struct/structPointer.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 type employee struct {
-    name   string
-    age    int
-    salary int
+	name   string
+	age    int
+	salary int
 }
 
 func main() {
-    empP := new(employee)
-    fmt.Printf("Emp Pointer Address: %p\n", empP)
-    fmt.Printf("Emp Pointer: %+v\n", empP)
-    fmt.Printf("Emp Value: %+v\n", *empP)
+	empP := new(employee)
+	fmt.Printf("Emp Pointer Address: %p\n", empP)
+	fmt.Printf("Emp Pointer: %+v\n", empP)
+	fmt.Printf("Emp Value: %+v\n", *empP)
+	*empP = employee{"John Doe", 30, 1000}
+	fmt.Printf("Emp After Change: %+v\n", *empP) // modify the value of the struct through the pointer
 }
