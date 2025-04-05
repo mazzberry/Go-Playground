b05a7263bb1c8be062fa88219601a22b488e0e56
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-05 10:25:32 +0330

    add: implement employee and address structs with example usage in nestedStruct.go

--- 0.CompositeDataTypes/10.Struct/10.Struct/nestedStruct.go
+++ 0.CompositeDataTypes/10.Struct/10.Struct/nestedStruct.go
@@ -1,2 +1,25 @@
-package main
+package main // 2.2.8
 
+import "fmt"
+
+type employee struct {
+	Name    string
+	Age     int
+	Salary  float64
+	address address
+}
+
+type address struct {
+	Street string
+	City   string
+	State string
+}
+
+
+func main() {
+	m_Adrress := address{Street: "hamase", City: "Tehran", State: "Iran"}
+	emp := employee{Name: "Mohammad", Age: 21, Salary: 15000.0, address: m_Adrress}
+	fmt.Printf("city: %s\n", emp.address.City)
+	fmt.Printf("state: %s\n", emp.address.State)
+	
+}
