91edf18e358679a196f7189eb802df94b803588e
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-10 14:22:31 +0330

    add: update taxSystem interface to return int and implement usaTax struct with tax calculation

--- 13.Interface/6.Interface/runTimePolymorphismWithInterface.go
+++ 13.Interface/6.Interface/runTimePolymorphismWithInterface.go
@@ -3,7 +3,7 @@ package main
 
 
 type taxSystem interface {
-	calculateTax()
+	calculateTax() int
 }
 
 type indianTax struct {
@@ -24,4 +24,39 @@ type singaporeTax struct {
 func (s *singaporeTax) calculateTax() int {
 	tax := (s.income * s.taxPercentage) / 100 
 	return tax
+}
+
+type usaTax struct{
+	taxPercentage int
+	income int
+}
+
+func (u *usaTax) calculateTax() int {
+	tax := (u.income * u.taxPercentage) / 100
+	return tax 
+}
+
+
+func main() {
+	indianTax := &indianTax{taxPercentage: 10,
+		 income: 100000,
+		}
+	singaporeTax := &singaporeTax{
+		taxPercentage: 15,
+		income: 200000,
+	}
+
+	taxSystem := []taxSystem{indianTax, singaporeTax}
+	totalTax := calculateTotalTax(taxSystem)
+	println("Total Tax: ", totalTax)
+}
+
+
+func calculateTotalTax(taxSystems []taxSystem) int {
+	var totalTax int
+	for _, t := range taxSystems {
+		totalTax += t.calculateTax() // در اینجا runtime polymorphism رخ می دهد
+	}
+	return totalTax
+
 }
