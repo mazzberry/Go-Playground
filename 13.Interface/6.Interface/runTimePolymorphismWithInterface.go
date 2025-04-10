c41338e43b50a815788620bf2c04d0a9e1529aa2
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-10 18:43:17 +0330

    style: format code for consistency in struct definitions and function implementations

--- 13.Interface/6.Interface/runTimePolymorphismWithInterface.go
+++ 13.Interface/6.Interface/runTimePolymorphismWithInterface.go
@@ -8,7 +8,7 @@ type taxSystem interface {
 
 type indianTax struct {
 	taxPercentage int
-	income int
+	income        int
 }
 
 func (i *indianTax) calculateTax() int {
@@ -18,39 +18,37 @@ func (i *indianTax) calculateTax() int {
 
 type singaporeTax struct {
 	taxPercentage int
-	income int
+	income        int
 }
 
 func (s *singaporeTax) calculateTax() int {
-	tax := (s.income * s.taxPercentage) / 100 
+	tax := (s.income * s.taxPercentage) / 100
 	return tax
 }
 
-type usaTax struct{
+type usaTax struct {
 	taxPercentage int
-	income int
+	income        int
 }
 
 func (u *usaTax) calculateTax() int {
 	tax := (u.income * u.taxPercentage) / 100
-	return tax 
+	return tax
 }
-
 
 func main() {
 	indianTax := &indianTax{taxPercentage: 10,
-		 income: 100000,
-		}
+		income: 100000,
+	}
 	singaporeTax := &singaporeTax{
 		taxPercentage: 15,
-		income: 200000,
+		income:        200000,
 	}
 
 	taxSystem := []taxSystem{indianTax, singaporeTax}
 	totalTax := calculateTotalTax(taxSystem)
 	fmt.Println("Total Tax: ", totalTax)
 }
-
 
 func calculateTotalTax(taxSystems []taxSystem) int {
 	var totalTax int
