b2e8b7cecf25d41e90db16b9733a4bb3a6268bca
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-12 10:27:00 +0330

    feat: implement cat struct with breath and walk methods

--- 13.Interface/8.Interface/interface.go
+++ 13.Interface/8.Interface/interface.go
@@ -8,7 +8,25 @@ type animal interface {
 }
 
 
+type cat string 
 
+func (c cat) breath() {
+	fmt.Println("cat breath")
+}
+
+
+func (c cat) walk() {
+	fmt.Println("cat walks")
+}
+
+
+func main() {
+	var a animal 
+
+	a = cat("smokey")
+	a.breath()
+	a.walk()
+}
 
 
 
