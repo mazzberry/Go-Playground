2be2c9d261244728dcfd7e6bf18e0f90f1872312
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-14 09:13:14 +0330

    feat: refactor main function and update test function signatures for clarity

--- 13.Interface/13.Interface/interface.go
+++ 13.Interface/13.Interface/interface.go
@@ -6,8 +6,16 @@ func main() {
 	test("this is string")
 	test("10")
 	test(true)
+
+	test2("this is string")
+	test2("10")
+	test2(true)
 }
 
-func test(a interface{}) {
+func test(a interface{}) { // any is an alias for interface{}
+	fmt.Printf("(%v, %T) \n", a, a)
+}
+
+func test2(a any) { // any is an alias for interface{}
 	fmt.Printf("(%v, %T) \n", a, a)
 }
