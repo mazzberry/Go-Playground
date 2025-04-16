fa143a3b21b011acd05e5951837992311478150e
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-16 23:35:01 +0330

    feat: enhance error handling in main function

--- 15.ErrorHandling/1.ErrorHandling/errorHandling.go
+++ 15.ErrorHandling/1.ErrorHandling/errorHandling.go
@@ -1,2 +1,16 @@
-package main
+package main // 2.6.1
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("non_existent_file.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	} else {
+		fmt.Println(file.Name() + "File opened successfully")
+	}
+}
 
