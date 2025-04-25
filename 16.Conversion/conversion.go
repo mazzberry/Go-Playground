dc73de32e3c77c5f1c5b86792144da8db48425db
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-25 22:16:12 +0330

    fix: correct variable name casing for totalSum in conversion.go

--- 16.Conversion/conversion.go
+++ 16.Conversion/conversion.go
@@ -5,11 +5,11 @@ import "fmt"
 
 func main() {
 
-	var totalsum int = 846
+	var totalSum int = 846
 	var number int = 19
 	var avg float32
 
-	avg = float32(totalsum) / float32(number)
+	avg = float32(totalSum) / float32(number)
 
 	fmt.Printf("Average = %v\n", avg) // %f instead of %v
 }
