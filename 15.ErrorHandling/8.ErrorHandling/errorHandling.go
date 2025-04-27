f2f2559c64b8476186bb7dc15cb196e245fe08d0
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-27 09:28:18 +0330

    feat: implement error handling and add example usage in errorHandling.go

--- 15.ErrorHandling/8.ErrorHandling/errorHandling.go
+++ 15.ErrorHandling/8.ErrorHandling/errorHandling.go
@@ -1,2 +1,29 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+type errorOne struct{}
+func (e errorOne) Error() string {
+	return "Error One happend"
+}
+
+func main() {
+	var err1 errorOne
+	err2 := do() 
+		if err1 == err2 {
+			fmt.Println("equality operator: Both errors are equal")
+		}
+		if errors.Is(err2, err1) {
+			fmt.Println("errors.Is: Both errors are equal")
+		}
+	}
+
+
+func do() error {
+	return errorOne{}
+
+}
+
