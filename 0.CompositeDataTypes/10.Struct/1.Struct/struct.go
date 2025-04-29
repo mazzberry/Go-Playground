0381a4bc6afcc28cd1427c45291a8a9b6703dd5c
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-29 11:17:44 +0330

    feat: add main function and employee instance creation in struct.go

--- 0.CompositeDataTypes/10.Struct/1.Struct/struct.go
+++ 0.CompositeDataTypes/10.Struct/1.Struct/struct.go
@@ -1,6 +1,6 @@
 package main
 
-
+import "fmt"
 
 type employee struct { // define a struct type for employee
 	name   string
@@ -17,3 +17,11 @@ var emp2 = employee{ // create an instance of employee with given values using p
  	age: 28,
   	salary: 6000.0,
   } 
+
+
+  func main() {
+	emp3 := new(employee) // create a pointer to an instance of employee
+	emp3.name = "ali" // it's not nessecary to initialize age in this case, as it's zero value by default.
+	emp3.salary = 7000.0
+	fmt.Printf("Employee 4: %+v\n", emp3)
+  }
