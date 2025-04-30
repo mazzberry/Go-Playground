830eaa55655dd53225bdc2a2398fe0479c8af091
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-30 15:24:57 +0330

    fix: change employee age type from int to uint8 and refactor person struct

--- 0.CompositeDataTypes/10.Struct/1.Struct/struct.go
+++ 0.CompositeDataTypes/10.Struct/1.Struct/struct.go
@@ -4,8 +4,14 @@ import "fmt"
 
 type employee struct { // define a struct type for employee
 	name   string
-	age    int
+	age    uint8
 	salary float32
+}
+
+type person struct {
+	FirstName string 
+	LastName string 
+	Age uint8
 }
 
 var emp = employee{} // create an instance of employee
@@ -24,4 +30,24 @@ var emp2 = employee{ // create an instance of employee with given values using p
 	emp3.name = "ali" // it's not nessecary to initialize age in this case, as it's zero value by default.
 	emp3.salary = 7000.0
 	fmt.Printf("Employee 4: %+v\n", emp3)
+
+	emp4 := newEmployee("Mohammadreza", 21, 18.000)
+	fmt.Printf("Employee 5 : %+v\n", emp4)
+
+	person5 := newPerson(person{FirstName: "Alireza", LastName: "Rezaei", Age: 25})
+	fmt.Printf("Person 5 : %+v\n", person5)
+
   }
+
+
+func newEmployee(name string, age uint8, salary float32) *employee {
+	return &employee{name: name, age: age, salary: salary}
+}
+
+func newPerson(newPerson person) *person {
+	if len(newPerson.FirstName) < 4 {
+		return nil
+	}
+	return &person{FirstName: newPerson.FirstName, LastName: newPerson.LastName, Age: newPerson.Age}
+}
+
