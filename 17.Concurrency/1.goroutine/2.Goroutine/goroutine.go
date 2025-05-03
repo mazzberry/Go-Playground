e3c247a20c20de592a57a91ad18382caec8b3e5d
Mazzberry <h.mohammadreza736@gmail.com>
2025-05-03 13:50:21 +0330

    fix: add missing newlines at the end of goroutine.go

--- 17.Concurrency/1.goroutine/2.Goroutine/goroutine.go
+++ 17.Concurrency/1.goroutine/2.Goroutine/goroutine.go
@@ -1,1 +1,25 @@
 package main // 3.2.1
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func main() {
+	go start()
+	fmt.Println("started")
+	time.Sleep(1 * time.Second)
+	fmt.Println("finished")
+}
+	
+
+func start() {
+	go start2()
+	fmt.Println("In goroutine")
+}
+
+func start2() {
+	fmt.Println("In goroutine 2")
+
+}
