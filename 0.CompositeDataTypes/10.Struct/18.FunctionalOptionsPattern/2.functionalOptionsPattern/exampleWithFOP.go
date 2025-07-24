0ee8738a4448f0b709563a23779b81a542257879
Mazzberry <h.mohammadreza736@gmail.com>
2025-07-24 04:26:23 +0330

    refactor: clean up formatting and improve readability in functional options implementation

--- 0.CompositeDataTypes/10.Struct/18.FunctionalOptionsPattern/2.functionalOptionsPattern/exampleWithFOP.go
+++ 0.CompositeDataTypes/10.Struct/18.FunctionalOptionsPattern/2.functionalOptionsPattern/exampleWithFOP.go
@@ -12,19 +12,30 @@ type Person struct {
 	HairColor string
 }
 
-type PersonOptions func(*Person)  
+type PersonOptions func(*Person)
 
 func main() {
+	person := NewPerson(SetName("Mohammadreza"), SetLastName("Hosseini"), SetAge(21), SetGender("Male"), SetHeight(184), SetWeight(120), SetHairColor("Brown"))
 
+	fmt.Printf("Person : %+v", person)
 }
 
-func SetName(name string) PersonOptions{
-	return func(person *Person){
-		person.Name = name
+func NewPerson(options ...PersonOptions) *Person {
+	person := &Person{Name: "Alireza", Age: 19}
+
+	for _, option := range options {
+		option(person)
+	}
+	return person
+}
+
+func SetName(lastName string) PersonOptions {
+	return func(person *Person) {
+		person.Name = lastName
 	}
 }
 
-func SetAge(age int) PersonOptions{
+func SetAge(age int) PersonOptions {
 	return func(person *Person) {
 		person.Age = age
 	}
@@ -36,28 +47,26 @@ func SetLastName(lastName string) PersonOptions {
 	}
 }
 
-func SetGender(gender string) PersonOptions{
+func SetGender(gender string) PersonOptions {
 	return func(person *Person) {
 		person.Gender = gender
 	}
 }
 
-func SetHeight(height float64) PersonOptions{
+func SetHeight(height float64) PersonOptions {
 	return func(person *Person) {
 		person.Height = height
 	}
 }
 
-func SetWeight(weight float64) PersonOptions{
+func SetWeight(weight float64) PersonOptions {
 	return func(person *Person) {
 		person.Weight = weight
 	}
 }
 
-func SetHairColor(hairColor string) PersonOptions{
+func SetHairColor(hairColor string) PersonOptions {
 	return func(person *Person) {
 		person.HairColor = hairColor
 	}
 }
-
-
