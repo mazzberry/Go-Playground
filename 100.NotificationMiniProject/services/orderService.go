bfb88740a95d2ffeaa91253b9f627f02edb95e8f
Mazzberry <h.mohammadreza736@gmail.com>
2025-07-27 01:39:52 +0330

    changed all the dependencies to reverse dependency (implementation of abstraction)

--- 100.NotificationMiniProject/services/orderService.go
+++ 100.NotificationMiniProject/services/orderService.go
@@ -7,22 +7,18 @@ import (
 )
 
 type OrderService struct {
-	EmailService *externalservices.EmailService
-	SmsService   *externalservices.SmsService
+	Notifier externalservices.Notifier
 }
 
-func (o *OrderService) CreateOrder(order *entities.Order) *entities.Order {
+func (orderService *OrderService) CreateOrder(order *entities.Order) *entities.Order {
 	fmt.Printf("Order created : %v", order)
+	orderService.Notifier = externalservices.NewNotifier(order.NotificationType)
 
-	o.SmsService.SendMessage(order)
-	o.EmailService.SendEmail(order)
+	orderService.Notifier.SendNotify(order.UserId, "Order created")
 
 	return order
 }
 
 func NewOrderService() *OrderService {
-	return &OrderService{
-		EmailService: externalservices.NewEmailService(),
-		SmsService:   externalservices.NewSmsService(),
-	}
+	return &OrderService{}
 }
