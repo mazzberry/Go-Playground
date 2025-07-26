7112f3d3285cdca7e290e049cd3a41ff4ac7ba5f
Mazzberry <h.mohammadreza736@gmail.com>
2025-07-27 01:41:55 +0330

    implemented the notifier (of what kind you want to receive Notification: Email or Sms)

---
+++ 100.NotificationMiniProject/externalservices/notifier.go
@@ -0,0 +1,20 @@
+package externalservices
+
+import (
+	"notification/entities"
+)
+
+type Notifier interface {
+	SendNotify(receiver string, message string)
+}
+
+func NewNotifier(notificationType entities.NotificationType) Notifier {
+	switch notificationType {
+	case entities.Sms:
+		return NewSmsService()
+	case entities.Email:
+		return NewEmailService()
+	default:
+		return NewNilNotifyService()
+	}
+}
