ccd41d9dc2b0cc9de8bb22da5a1d1b704f0601b8
Mazzberry <h.mohammadreza736@gmail.com>
2025-07-27 01:40:42 +0330

    Added for testing

---
+++ 100.NotificationMiniProject/externalservices/nilNotifyService.go
@@ -0,0 +1,15 @@
+package externalservices
+
+import "fmt"
+
+type NilNotifyService struct {
+}
+
+func (e *NilNotifyService) SendNotify(receiver string, message string) {
+	fmt.Printf("nilNotifyService: Receiver: %s, Message: %s", receiver, message)
+}
+
+
+func NewNilNotifyService() *NilNotifyService {
+	return &NilNotifyService{}
+}
