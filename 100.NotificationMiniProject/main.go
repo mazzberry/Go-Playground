636386adcde8e2560f6b484b03039695dea632ef
Mazzberry <h.mohammadreza736@gmail.com>
2025-07-27 01:34:36 +0330

    created 2 new instance for testing

--- 100.NotificationMiniProject/main.go
+++ 100.NotificationMiniProject/main.go
@@ -3,20 +3,43 @@ package main
 
 import (
 	"notification/entities"
+	//"notification/externalservices"
 	"notification/services"
 )
 
 func main() {
-	order := entities.Order{
-		Id:           1,
-		UserFullName: "John Doe",
-		UserEmail:    "john@gmail.com",
-		UserPhone:    "9114642212",
-		Price:        2000,
-		Status:       true,
+	order1 := entities.Order{
+		Id:               1,
+		UserFullName:     "John Doe",
+		UserId:           "09134212531",
+		Price:            2000,
+		Status:           true,
+		NotificationType: entities.Email,
+	}
+
+	order2 := entities.Order{
+		Id:               2,
+		UserFullName:     "mahbod akbari",
+		UserId:           "09132341231",
+		Price:            4000,
+		Status:           true,
+		NotificationType: entities.Sms,
+	}
+
+	order3 := entities.Order{
+		Id:               2,
+		UserFullName:     "masoud rajavi",
+		UserId:           "099990341231",
+		Price:            4000,
+		Status:           true,
+		NotificationType: entities.Nil,
 	}
 
 	orderService := services.NewOrderService()
-	orderService.CreateOrder(&order)
+	orderService.CreateOrder(&order1)
+
+	orderService.CreateOrder(&order2)
+
+	orderService.CreateOrder(&order3)
 
 }
