aa420c3c4698a68793d7b99c5c3e8e92d78896ed
Mazzberry <h.mohammadreza736@gmail.com>
2025-07-31 07:34:53 +0330

    create an example of module

---
+++ 14.Modules/2.example/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	jalaali "github.com/jalaali/go-jalaali"                                      //outer module
+	"github.com/mazzberry/Go-Playground/tree/main/14.Modules/2.example/services" //inner package
+)
+
+func main() {
+	fmt.Printf("Hello world!\n")
+
+	year, month, day, err := jalaali.ToGregorian(1401, 1, 34)
+	if err == nil {
+		fmt.Printf("%d %d %d\n", year, month, day)
+	} else {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	var service services.TestService = services.TestService{}
+
+	var service2 services.TestService2 = services.TestService2{}
+
+	fmt.Printf("%v", service)
+	fmt.Printf("%v", service2)
+
+}
