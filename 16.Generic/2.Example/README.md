cd4d70aed9ae961c8ebab7de52cc0ee6a096d1d9
Mazzberry <h.mohammadreza736@gmail.com>
2025-08-02 12:26:47 +0330

    write an example of implementation of generics , this type [by Slices]

---
+++ 16.Generic/2.Example/README.md
@@ -0,0 +1,8 @@
+![alt text](image.png)
+
+it's right that we could implement the generic like this in the screenshot <br>
+but, to have cleaner code we can also implement it with an interface that i shown it blow 
+
+![alt text](image-1.png)
+
+![alt text](image-2.png)
---
+++ 16.Generic/2.Example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Number interface{
+	int | int16 | int32 | int64 | uint | uint16 | uint32 | uint64 | float32 | float64
+}
+
+
+func main() {
+	mySlice := []int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("%d\n", SumInt(mySlice))
+
+	generic1 := []float64{1.3, 4.7, 6.6, 8.1, 9.2}
+	fmt.Printf("%v\n", Sum(generic1))
+
+	generic2 := []float64{3, 4, 7, 5, 2, 2, 1, 8}
+	fmt.Printf("%v\n", Sum(generic2))
+
+}
+
+func SumInt(slc []int) (sum int) {
+	for _, v := range slc {
+		sum += v
+	}
+	return
+}
+
+func Sum[T int | float64](slc []T) (sum T) {
+	for _, v := range slc {
+		sum += v
+	}
+	return
+}
