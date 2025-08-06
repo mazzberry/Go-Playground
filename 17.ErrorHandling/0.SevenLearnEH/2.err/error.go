4aa9d17e28035666d55932b69d526ac6aca1b8a2
Mazzberry <h.mohammadreza736@gmail.com>
2025-08-06 20:51:40 +0330

    created an error and handle it

--- 17.ErrorHandling/0.SevenLearnEH/2.err/error.go
+++ 17.ErrorHandling/0.SevenLearnEH/2.err/error.go
@@ -6,27 +6,34 @@ import (
 )
 
 func main() {
-	x, err := createErrorMethod1(0)
-	fmt.Println(x, err)
+	output, err := createErrorMethod1(2)
 
-	x, err = createErrorMethod2(0)
-	fmt.Println(x, err)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(output)
+
+	// fmt.Println(createErrorMethod1(0))
+	// fmt.Println(createErrorMethod2(0))
+
+	// fmt.Println(createErrorMethod1(10))
+	// fmt.Println(createErrorMethod1(10))
+
 }
 
-func createErrorMethod1(number int) (int, error){
+func createErrorMethod1(number int) (int, error) {
 	if number == 0 {
 		return 0, errors.New("M1: number is not valid")
 	}
-	err := errors.New("and error has occurred")
-	fmt.Println(err)
+	
 	return number * 6, nil
 }
 
-func createErrorMethod2(number int) (int, error){
+func createErrorMethod2(number int) (int, error) {
 	if number == 0 {
 		return 0, fmt.Errorf("M2: number is not valid: %d", number)
 	}
-	err := errors.New("and error has occurred")
-	fmt.Println(err)
+	
 	return number * 6, nil
 }
