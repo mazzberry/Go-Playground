c3101e8aa2b37483979083c5cf177e2444148626
Mazzberry <h.mohammadreza736@gmail.com>
2025-10-14 09:48:48 +0330

    create log example

---
+++ 18.Loging/1.Loglib/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func init() {
+	file, err := os.OpenFile("log.txt", os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalln("something went wrong with log file", err)
+	}
+	log.SetOutput(file)
+}
+
+func main() {
+	log.Println("start of main func\n")
+	sum(2, 3)
+	log.Println("\nend main func\n")
+
+}
+
+func sum(a, b int) {
+	log.Println("start of sum\n")
+	fmt.Println(a + b)
+	log.Println("\nend of the sum operation\n")
+}
